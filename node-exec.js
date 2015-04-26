407f81d236c655ccacf668d360ea9fb98d88c81f
FrankBowers24 <FrankBowers24@gmail.com>
2015-04-25 20:05:20 -0700

    Use prompt node-module for getting email and password

--- node-exec.js
+++ node-exec.js
@@ -1,61 +1,24 @@
-var sys = require('sys');
-var readline = require('readline');
 var exec = require('child_process').exec;
 var child;
+var prompt = require('prompt');
 
+prompt.start();
 
-var get_password = function (callback) {
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.setRawMode(true);
-  password = ''
-  process.stdin.on('data', function (char) {
-    char = char + ""
-
-    switch (char) {
-      case "\n":
-      case "\r":
-      case "\u0004":
-        // They've finished typing their password
-        process.stdin.setRawMode(false);
-        callback(password);
-        // console.log("\nyou entered: " + password)
-        process.stdin.pause()
-        break
-      case "\u0003":
-        // Ctrl C
-        console.log('Cancelled')
-        process.exit()
-        break
-      default:
-        // More passsword characters
-        process.stdout.write('*')
-        password += char
-        break
+var schema = {
+  properties: {
+    Email: {
+      required: true
+    },
+    Password: {
+      hidden: true
     }
-  });
-}
+  }
+};
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
 
-console.log("Enter your LinkedIn Email: ")
-rl.on('line', function (email) {
-  // console.log("Email: ", email);
-  rl.close();
-  console.log("Enter Your LinkedIn Password: ");
-  get_password(function (password) {
-  	console.log("");
-    child = exec("phantomjs echo-args.js " + email + " " + password,
-      function (error, stdout, stderr) {
-        console.log(stdout);
-        // console.log('stderr: ' + stderr);
-        // if (error !== null) {
-        //   console.log('exec error: ' + error);
-        // }
-      });
-  });
+prompt.get(schema, function (err, result) {
+  child = exec("phantomjs echo-args.js " + result.Email + " " + result.Password,
+    function (error, stdout, stderr) {
+      console.log(stdout);
+    });
 });
